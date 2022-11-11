b7b628a677fe959c1bb4d65975b53d7236b30cf6
gabreilAlexKH <85039004+gabreilAlexKH@users.noreply.github.com>
2022-11-11 18:11:27 -0500

    Nombres de Clases y datos primitivos

--- lexen.py
+++ lexen.py
@@ -6,16 +6,80 @@ reservadas = {
   
 }
 
-#Tipos de datos primitivos y variables
+#Clases y nombres de datos primitivos. Encargado: Gabriel Maldonado
+clases = {
+  'Int':'INTCLASS',
+  'Long':'LONGCLASS',
+
+  'Double':'DOUBLECLASS',
+  'Float':'FLOATCLASS',
+
+  'Char':'CHARCLASS',
+  'String':'STRINGCLASS',
+
+  'List':'LISTCLASS',
+  'Array':'ARRAYCLASS',
+  'Boolean':"BOOLCLASS"
+}
+
+#Tipos de datos primitivos y variables. Encargado: Gabriel Maldonado
 
 datos = [ 
+  "INT",
+  "LONG",
+  
+  "DOUBLE",
+  "FLOAT",
+  
+  "STRING",
+  "CHAR",
+  
+  "VARIABLE",
 ]
 
+
+def t_FLOAT(t):
+  r'\d*\.\d+(f|F)'
+  t.value = float(t.value[:-1])
+  return t
+
+def t_DOUBLE(t):
+  r'\d*\.\d+?'
+
+  t.value = float(t.value)
+  return t
+
+def t_LONG(t):
+  r'\d+(l|L)'
+  t.value = int(t.value[:-1])
+  return t
+ 
+def t_INT(t):
+  r'\d+'
+  t.value = int(t.value)
+  return t
+
+
+def t_STRING(t):
+  r'"(.*|\s)*"'
+  t.value = t.value
+  return t
+
+def t_CHAR(t):
+  r'\'.\''
+  t.value = t.value
+  return t
+
+def t_VARIABLE(t):
+  r'[a-zA-Z][a-zA-Z0-9]*(_)?[a-zA-Z0-9]*'
+  t.type = reservadas.get(t.value, 'VARIABLE')# Check palabras reservadas
+  t.type = clases.get(t.value, 'VARIABLE')  # Check nombres de clases
+  return t
 
 
 #Simbolos de cierre ,  simbolos de putuacion y commilas
 
-simbolo =[
+simbolo =[ 
   
 ]
 
@@ -30,11 +94,10 @@ operadores = [
 #Funciones
 funciones = []
 
-#Clases y nombres de datos primitivos
-clases = []
+
 
 #tokens
-tokens = list(reservadas.values()) + datos + operadores + simbolo + relocalizar + clases
+tokens = list(reservadas.values()) + datos + operadores + simbolo + funciones + list(clases.values())
 
 
 
@@ -63,7 +126,7 @@ lexer = lex()
 def getTokens(lexer):
   for tok in lexer:
     print(tok)
-
+'''
 #Lectura de archivo 
 print('Mi primer Lexer')
 file = open("source.scala")
@@ -71,6 +134,7 @@ archivo = file.read()
 file.close()
 lexer.input(archivo)
 getTokens(lexer)
+'''
 
 
 print('Mi primer Lexer')
